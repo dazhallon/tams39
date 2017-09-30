08bf8d8d83ea5a0acc9ed1970115f2a37617a789
dazhallon <antka388 @student.liu.se>
2017-09-30 09:12:04 +0200

    added data to ex4.m

--- examination-exercises/ex4.m
+++ examination-exercises/ex4.m
@@ -1,4 +1,87 @@
-% exercise 4
+				% exercise 4
+
+%% read data
+%  Ch. Concinna
+X1 = [191  131  53  150  15  104;
+    185  134  50  147  13  105;
+    200  137  52  144  14  102;
+    173  127  50  144  16  97;
+    171  118  49  153  13  106;
+    160  118  47  140  15  99;
+    188  134  54  151  14  98;
+    186  129  51  143  14  110;
+    174  131  52  144  14  116;
+    163  115  47  142  15  95;
+    190  143  52  141  13  99;
+    174  131  50  150  15  105;
+    201  130  51  148  13  110;
+    190  133  53  154  15  106;
+    182  130  51  147  14  105;
+    184  131  51  137  14  95;
+    177  127  49  134  15  105;
+    178  126  53  157  14  116;
+    210  140  54  149  13  107;
+    182  121  51  147  13  111;
+    186  136  56  148  14  111];
+
+%  Ch. Heikertlinger
+X2= [186  107  49  120  14  84;
+    211  122  49  123  16  95;
+    201  114  47  130  14  74;
+    242  131  54  131  16  90;
+    184  108  43  116  16  75;
+    211  118  51  122  15  90;
+    217  122  49  127  15  73;
+    223  127  51  132  16  84;
+    208  125  50  125  14  88;
+    199  124  46  119  13  78;
+    211  129  49  122  13  83;
+    218  126  49  120  15  85;
+    203  122  49  119  14  73;
+    192  116  49  123  15  90;
+    195  123  47  125  15  77;
+    211  122  48  125  14  73;
+    187  123  47  129  14  75;
+    192  109  46  130  13  90;
+    223  124  53  129  13  82;
+    188  114  48  122  12  74;
+    216  120  50  129  15  86;
+    185  114  46  124  15  92;
+    178  119  47  120  13  78;
+    187  111  49  119  16  66;
+    187  112  49  119  14  55;
+    201  130  54  133  13  84;
+    187  120  47  121  15  86;
+    210  119  50  128  14  68;
+    196  114  51  129  14  86;
+    195  110  49  124  13  89;
+    187  124  49  129  14  88];
+
+% Ch. Heptapatamica
+X3 = [158  141  58  145  8   107;
+    146  119  51  140  11  111;
+    151  130  51  140  11  113;
+    122  113  45  131  10  102;
+    138  121  53  139  11  106;
+    132  115  49  139  10  98;
+    131  127  51  136  12  107;
+    135  123  50  129  11  107;
+    125  119  51  140  10  110;
+    130  120  48  137  9   106;
+    130  131  51  141  11  108;
+    138  127  52  138  9   101;
+    130  116  52  143  9   111;
+    143  123  54  142  11  95;
+    154  135  56  144  10  123;
+    147  132  54  138  10  102;
+    141  131  51  140  10  106;
+    131  116  47  130  9   102;
+    144  121  53  137  11  104;
+    137  146  53  137  10  113;
+    143  119  53  136  9   105;
+    135  127  52  140  10  108];
+
+data = {X1, X2, X3};
 disp('(a) test if the covariance matrices can be pooled')
 % create covariance matrices, and create the pooled matrix
 k = 3;
@@ -6,12 +89,13 @@ p = size(data{1}, 2);
 ns = zeros(k,1);
 S = cell(k,1);
 Sp = zeros(p,p);
+means = zeros(p, k);
 for i =1:k
   S{i} = cov(data{i});
   ns(i) =  size(data{i},1);
   fs(i) = ns(i) -1;
   Sp = Sp + (ns(i) -1)*S{i};
-  means(i) = mean(data{i}, 1);
+  means(:,i) = mean(data{i}, 1);
 end
 n = sum(ns);
 f = sum(fs);
@@ -21,14 +105,16 @@ Sp = Sp/(n-1);
 Vs = cell(k,1);
 V = zeros(p,p);
 for i = 1:k
-  Vs{i} = data{i}*(eye(ns(i)) - ns(i)^-1*ones(ns(1)));
+  Vs{i} = data{i}'*(eye(ns(i)) - ns(i)^-1*ones(ns(i)))*data{i};
   V = V + Vs{i};
 end 
 
 
-lambdastar  = prod(Vs{:}.^(fs(:)/2)) / det(V)^(f/2) ...
-  * f^(p*f/2) / prod(fs(:).^(p*fs(:)/2));
-
+loglambdastar  = (p*f/2)*log(f) - (f/2)*log(det(V));
+for i = 1:k
+  loglambdastar  = loglambdastar + (fs(i)/2)*log(det(Vs{i})) ...
+    - (p*fs(i)/2)*log(fs(i));
+end
 df = 0.5*p*(p+1)*(k-1);
 alpha = 0;
 for i = 1:k
@@ -38,7 +124,7 @@ alpha  = alpha -1;
 alpha = alpha * (2*p^2 + 3*p - 1)/(12*(p+1)*(k-1));
 m = f- 2*alpha;
 
-test = -2*f^-1 * m *log(lambdastar);
+test = -2*f^-1 * m * loglambdastar
 
 c = chi2inv(0.95, df)
 
@@ -57,9 +143,9 @@ disp('(b) Derive the classification rule for these data')
 ls = zeros(k, p);
 invSp = inv(Sp);
 
-if i = 1:k
-  ls(i,:) = means(i) * invSp
-  c(i) = -0.5*means(i)'*invSp*means(i);
+for i = 1:k
+  ls(i,:) = means(:,i)' * invSp;
+  c(i) = -0.5*means(:,i)'*invSp*means(:,i);
 end
 
 x0 = zeros(p,1);
@@ -69,16 +155,16 @@ class = 1;
 L = @(x, l, c) l * x + c;
 max = L(x0, ls(1), c(1));
 for i = 2:k
-  tmp = L(x0, ls(i) c(i));
+  tmp = L(x0, ls(i), c(i));
   if tmp > max
     max = tmp;
     class = i;
   end
 end
 
-disp(['We can classify x0 as ', num2str(i)]);
+disp(['We can classify x0 as pi_', num2str(i)]);
 
-disp('(c) Estimate the errors of misclassification of ')
+disp('(c) Estimate the errors of misclassification of... ')
 
 % how to estimate the errors whem we have three classes.instead
 % of just 2?
